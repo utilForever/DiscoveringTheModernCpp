9d63597be48c38e65b05fa9308375e30e0aaee82
Chris Ohk <utilForever@gmail.com>
2015-12-26 11:26:44 +0900

    Create first program

---
+++ DiscoveringTheModernCpp/Chapter 1/FirstProgram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "The answer to the Ultimate Question of Life, \n"
+		<< "the Universe, and Everything is: "
+		<< std::endl << 6 * 7 << std::endl;
+
+	return 0;
+}
