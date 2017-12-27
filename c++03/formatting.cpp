0a66692bc44ed1b38a55930e33e97ed66111f5ad
Chris Ohk <utilforever@gmail.com>
2017-12-28 01:22:20 +0900

    1장 예제 주석 한글화

--- c++03/formatting.cpp
+++ c++03/formatting.cpp
@@ -2,40 +2,35 @@
 #include <iomanip>
 #include <cmath>
 
-
-
-int main (int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-    using namespace std;
-    double pi= M_PI;
-    
-    setprecision(3);
+	using namespace std;
+	double pi = M_PI;
 
-    int old_precision= cout.precision();
-    cout << "pi is " << pi << '\n';
-    cout << "pi is " << setprecision(16) << pi << '\n';
-    cout << "pi is " << setw(30) << pi << '\n';
-    cout << "pi is " << setfill('-') << left << setw(30) << pi << '\n';
-    cout.setf(ios_base::showpos);
-    cout << "pi is " << scientific << pi << '\n';
-    cout << "pi is " << right << fixed << setw(30) << pi << '\n';
-   
-    cout << "63 octal is " << oct << 63 << '\n';
-    cout << "63 hexadecimal is " << hex << 63 << '\n';
-    cout << "63 decimal is " << dec << 63 << '\n';
-    cout << "pi is " << pi << '\n';
+	setprecision(3);
 
-    cout << "pi < 3 is " << (pi < 3) << '\n';
-    // cout.setf(ios_base::boolalpha);
-    cout << "pi < 3 is " << boolalpha << (pi < 3) << '\n';
+	int old_precision = cout.precision();
+	cout << "pi is " << pi << '\n';
+	cout << "pi is " << setprecision(16) << pi << '\n';
+	cout << "pi is " << setw(30) << pi << '\n';
+	cout << "pi is " << setfill('-') << left << setw(30) << pi << '\n';
+	cout.setf(ios_base::showpos);
+	cout << "pi is " << scientific << pi << '\n';
+	cout << "pi is " << right << fixed << setw(30) << pi << '\n';
 
-    // clear all formatting
-    cout.unsetf(ios_base::adjustfield | ios_base::basefield | ios_base::floatfield 
-		| ios_base::showpos | ios_base::boolalpha);
-    cout.precision(old_precision);
-    cout << "pi is " << pi << '\n';
+	cout << "63 octal is " << oct << 63 << '\n';
+	cout << "63 hexadecimal is " << hex << 63 << '\n';
+	cout << "63 decimal is " << dec << 63 << '\n';
+	cout << "pi is " << pi << '\n';
 
+	cout << "pi < 3 is " << (pi < 3) << '\n';
+	// cout.setf(ios_base::boolalpha);
+	cout << "pi < 3 is " << boolalpha << (pi < 3) << '\n';
 
-    return 0;
+	// 모든 서식 설정 지우기
+	cout.unsetf(ios_base::adjustfield | ios_base::basefield | ios_base::floatfield | ios_base::showpos | ios_base::boolalpha);
+	cout.precision(old_precision);
+	cout << "pi is " << pi << '\n';
+
+	return 0;
 }
-
--- c++11/overload_testing.cpp
+++ c++11/overload_testing.cpp
@@ -3,19 +3,19 @@
 using std::cout;
 
 // void f(int x) { cout << "int\n"; }
-// void f(int y) { cout << "int\n"; } // Redefinition: only arg name different
-// long f(int x) { cout << "int\n"; } // Redefinition: only return type different
+// void f(int y) { cout << "int\n"; } // 재정의: 인수의 이름만 다르다.
+// long f(int x) { cout << "int\n"; } // 재정의: 리턴 타입만 다르다.
 // void f(int&& x) { cout << "int&&\n"; }
-// void f(const int&& x) { cout << "const int&&\n"; } // superseded by int&&
+// void f(const int&& x) { cout << "const int&&\n"; } // int&&로 대체된다.
 void f(int& x) { cout << "int&\n"; }
 void f(const int& x) { cout << "const int&\n"; }
 
-int main (int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-    int       i= 3;
-    const int ci= 4;
+	int i = 3;
+	const int ci = 4;
 
-    f(3);
-    f(i);
-    f(ci);
+	f(3);
+	f(i);
+	f(ci);
 }
